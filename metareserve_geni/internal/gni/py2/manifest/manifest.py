2b1e4ddbbcb5984e2589ec4822a8b2694523ad7e
Sebastiaan Alvarez Rodriguez van Ravensberg <sebastiaanalva@gmail.com>
2021-07-25 11:55:04 +0200

    Minor bug fix

--- metareserve_geni/internal/gni/py2/manifest/manifest.py
+++ metareserve_geni/internal/gni/py2/manifest/manifest.py
@@ -17,16 +17,10 @@ class Manifest(object):
     def __init__(self, manifest, do_parse=True):
         self.data = xmltodict.parse(manifest.text) if do_parse else manifest
         if self.__has_indices('rspec', 'node', 0):
-            self.__type = ManifestType.CREATE_SLIVER
             self.__num_nodes = len(self.data['rspec']['node'])
         else:
-            self.__type = ManifestType.RENEW_OR_LIST_SLIVER
             self.__num_nodes = 1
 
-
-    @property
-    def manifest_type(self):
-        return self.__type
 
     @property
     def num_nodes(self):
@@ -64,17 +58,19 @@ class Manifest(object):
 
     def get_connect_info(self):
         '''Returns iterable of `RawConnectInfo`:(name, user, ip_local, ip_public, port) for all found nodes'''
-        if self.__type == ManifestType.CREATE_SLIVER:
+        if self.__num_nodes > 1:
             return [RawConnectInfo(
                 str(self.data['rspec']['node'][idx]['@client_id']),
                 str(self.data['rspec']['node'][idx]['services']['login']['@username']),
                 str(self.data['rspec']['node'][idx]['interface']['ip']['@address']),
                 str(self.data['rspec']['node'][idx]['host']['@ipv4']),
                 str(self.data['rspec']['node'][idx]['services']['login']['@port'])) for idx in range(self.__num_nodes)]
-        else:
+        elif self.__num_nodes == 1:
             name = str(self.data['rspec']['node']['@client_id'])
             user = str(self.data['rspec']['node']['services']['login']['@username'])
-            ip_local = str(self.data['rspec']['node'][idx]['interface']['ip']['@address'])
+            ip_local = str(self.data['rspec']['node']['interface']['ip']['@address'])
             ip_public = str(self.data['rspec']['node']['host']['@ipv4'])
             port = str(self.data['rspec']['node']['services']['login']['@port'])
             return [RawConnectInfo(name, user, ip_local, ip_public, port)]
+        else:
+            raise RuntimeError('No nodes found!')
